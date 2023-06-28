bfbdba297fe6d49e1f40784cb99c95b374a29bfe
bhabesh2 <jsarma058@gmail.com>
2023-06-28 12:50:32 +0530

    solution added

---
+++ express/problem3/file.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ msg: 'I am homepage' });
+});
+
+app.get('/about', (req, res) => {
+  res.json({ msg: 'I am about page' });
+});
+
+app.get('/contact', (req, res) => {
+  res.json({ email: 'support@pwskills.com' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
